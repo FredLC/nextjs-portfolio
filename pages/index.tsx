c0e5185d0444a81b245ad42a59c688c69a2a6c0e
Fred Lefebvre <fredlefevre@outlook.com>
2022-12-20 13:37:22 -0500

    Implemented hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import Header from '../components/Header'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Fred's Portfolio</title>
         <meta name="description" content="Fred Lefebvre Portfolio" />
@@ -16,7 +17,10 @@ export default function Home() {
       </Head>
 
       <Header />
-      {/* Hero */}
+
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
 
       {/* About */}
 
@@ -27,7 +31,6 @@ export default function Home() {
       {/* Projects */}
 
       {/* Contact Me */}
-
-    </>
-  )
+    </div>
+  );
 }
